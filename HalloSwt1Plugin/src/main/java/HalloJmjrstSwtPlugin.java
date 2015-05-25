e778688a45a43d90aec61c0c7d9fd6d408ad8747
IrisLanderer <iris.landerer@gmail.com>
2015-05-25 15:57:24 +0200

    mein erstes Plugin

---
+++ HalloSwt1Plugin/src/main/java/HalloJmjrstSwtPlugin.java
@@ -0,0 +1,42 @@
+import org.jis.Main;
+import org.jis.plugins.JmjrstPlugin;
+
+public class HalloJmjrstSwtPlugin extends JmjrstPlugin {
+
+	@Override
+	public String getMenuText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(Main main) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isConfigurable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void configure() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
